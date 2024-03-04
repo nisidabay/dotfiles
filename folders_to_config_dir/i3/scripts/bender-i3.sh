b0cd449a92af551cb8cfcb22b05a1fd26964099d
nisidabay <nisidabay@gmail.com>
2024-03-04 10:53:36 +0100

    Modify folders_to_config_dir/i3/scripts/bender-i3.sh

--- folders_to_config_dir/i3/scripts/bender-i3.sh
+++ folders_to_config_dir/i3/scripts/bender-i3.sh
@@ -1,43 +1,13 @@
-#!/usr/bin/bash
+#!/bin/bash
 # Idea from: Internet
-# i3 screen-lock with random images
+# i3 screen-lock with a clock
 # Modified by CLM and ChatGPT
-# Description: Lock screen with random images
+# Description: Lock screen with the a clock
 # Date: mar 17 ene 2023 01:35:18 CET
 # Modified date: dom 31 dic 2023 08:25:51 CET
 # Dependencies: yay -S i3lock-color
 ##############################################################################
 
-# Declare variables for directory path and file types
-img_dir=~/Templates/
-file_types=(jpg png)
-
-# Declare an empty array for images
-images=()
-
-# Function to delete the screenshot
-delete_screenshot() {
-    unlink /tmp/locking_screen.png
-    unlink /tmp/screen_blur.png
-    unlink /tmp/screen.png
-}
-
-# Trap to delete the screenshot when the script is interrupted
-trap delete_screenshot SIGINT SIGHUP
-
-# Function to get a list of all image files in the directory
-get_images() {
-    for file in "$img_dir"/*."${file_types[@]}"; do
-        if [ -f "$file" ]; then
-            images+=("$file")
-        fi
-    done
-
-    if [ ${#images[@]} -eq 0 ]; then
-        echo "No image files found in $img_dir"
-        exit 1
-    fi
-}
 
 # Function to capture and blur the desktop image
 capture_desktop() {
@@ -45,18 +15,9 @@ capture_desktop() {
     convert -blur 0x8 /tmp/locking_screen.png /tmp/screen_blur.png
 }
 
-# Function to composite a random image on top of the blurred desktop image
-composite_image() {
-    random_image=$(shuf -n 1 -e "${images[@]}")
-    convert -composite /tmp/screen_blur.png "$random_image" -gravity Center -geometry -20x1200 /tmp/screen.png
-}
-
-# Function to lock the screen with the new image
+# Function to lock the screen 
 lock_screen() {
-    # Comment the line below and uncomment the i3lock -i /tmp/screen.png to
-    # screen an image on top of the lock screen
     style_lock
-    # i3lock -i /tmp/screen.png
 }
 
 style_lock() {
@@ -98,18 +59,7 @@ i3lock \
 --keylayout 1                
 }
 
-# Get a list of all image files in the directory
-get_images
-
-# Capture and blur the desktop image
 capture_desktop
-
-# Composite a random image on top of the blurred desktop image
-composite_image
-
-# Lock the screen with the new image
 lock_screen
 
-# Delete the screenshot
-delete_screenshot
 
