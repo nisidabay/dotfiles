058acf863ef53d7227983cc750365ac02476c77b
nisidabay <nisidabay@gmail.com>
2024-06-09 11:57:03 +0200

    Add config files

---
+++ folders_to_config_dir/cmus/lib.pl
@@ -0,0 +1,18 @@
+/home/nisidabay/Music/Still Counting - Volbeat.mp3
+/home/nisidabay/Music/Harry_Potter-Camera_dei_segreti.mp3
+/home/nisidabay/Music/Harry_Potter-Il_Prigioniero_di_Azkaban2.mp3
+/home/nisidabay/Music/BLACK SABBATH - ＂Children of the Grave＂ from The End (Live Video).mp3
+/home/nisidabay/Music/Deep Purple ： Gettin' Tighter (Come Taste The Band Tour).mp3
+/home/nisidabay/Music/Dream Evil.mp3
+/home/nisidabay/Music/Lock up the wolves.mp3
+/home/nisidabay/Music/Epic_Music-1.mp3
+/home/nisidabay/Music/Alessandro Magno-1.mp3
+/home/nisidabay/Music/Alessandro Magno-2.mp3
+/home/nisidabay/Music/Alessandro Magno-3.mp3
+/home/nisidabay/Music/Alessandro Magno-4.mp3
+/home/nisidabay/Music/Hughes⧸Thrall  1982 [Full Album] Remastered 2007.mp3
+/home/nisidabay/Music/Led Zeppelin - Babe I'm Gonna Leave You (Official Audio).mp3
+/home/nisidabay/Music/Led Zeppelin - Since I've Been Loving You (Live at Madison Square Garden 1973) [Official Video].mp3
+/home/nisidabay/Music/Meditation-1.mp3
+/home/nisidabay/Music/Dave-ringtone.mp3
+/home/nisidabay/Music/ZZ Top - La Grange (Live From Gruene Hall) ｜ Stages.mp3
